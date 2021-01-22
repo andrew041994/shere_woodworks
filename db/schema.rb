58ab5fb68f4189c4d1a07abc3ecf8344f39b7314
andrew041994 <andrew041994@gmail.com>
2021-01-21 23:02:21 -0500

    added devise to admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_22_034614) do
+ActiveRecord::Schema.define(version: 2021_01_22_035640) do
 
-  create_table "owners", force: :cascade do |t|
+  create_table "admins", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.string "username"
-    t.string "email"
-    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
   create_table "users", force: :cascade do |t|
