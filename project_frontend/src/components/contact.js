d2f25386999aa04e302ef2d6911ff32137fcbb52
andrew041994 <andrew041994@gmail.com>
2021-03-15 18:44:24 -0400

    contact form

--- project_frontend/src/components/contact.js
+++ project_frontend/src/components/contact.js
@@ -1,19 +1,54 @@
 import React, { Component } from 'react';
+import emailjs from 'emailjs-com'
+import { Link } from 'react-router-dom'
+import Button from 'react-bootstrap/Button'
+
 
 export default class Contact extends Component {
-    constructor() {
-        super();
-        this.state = {
-            username: "",
-            password: ""
-        }
+  constructor() {
+    super();
+
+    this.state = {
+      name: "",
+      subject: "",
+      email: "",
+      message: "",
     }
+    this.sendEmail = this.sendEmail.bind(this);
 
+  }
+  
 
+sendEmail(e) {
+    e.preventDefault();
+    emailjs.sendForm('service_ngumvtj', 'template_mr6phfo', e.target, 'user_rjqNpqutQge0Nf647Xr3R')
+    .then((result) => {
+      console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      })
+      this.setState({
+        name: "",
+      subject: "",
+      email: "",
+      message: ""
+      })
+  }
 
   render() {
-    return (
-      <div> textInComponent </div>
-    );
-  }
+  return (
+    <div style={{ margin: "auto", width: "20%", padding: "30px" }}>
+
+      <form className="contact-form" onSubmit={this.sendEmail}>
+        <input required="required" type="text" name="name" placeholder="Name" value={this.state.name} onChange={(event) => this.setState({name: event.target.value})}/> <br/><br/>
+        <input required="required" type="text" name="subject" placeholder="Subject" value={this.state.subject} onChange={(event) => this.setState({subject: event.target.value})}/><br/><br/>
+        <input required="required" type="email" name="email" placeholder="Email" value={this.state.email} onChange={(event) => this.setState({email: event.target.value})} /><br/><br/>
+        <textarea required="required" type="textarea" name="message" placeholder="Message" value={this.state.message} onChange={(event) => this.setState({message: event.target.value})} /><br/><br/>
+        <Button variant="success"> Submit </Button>
+        <Link to="/"><Button variant="warning"  >Home</Button> 
+      </Link>
+      </form> 
+    </div>
+  );
+}
 }
