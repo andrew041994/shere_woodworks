78f0eaf000f161c3e6c506bc29e8c4524d9da0d7
andrew041994 <andrew041994@gmail.com>
2021-03-18 00:20:41 -0400

    contact form validations

--- project_frontend/src/components/contact.js
+++ project_frontend/src/components/contact.js
@@ -42,27 +42,27 @@ sendEmail(e) {
         <Container fluid>
           <Row className="justify-content-md-center">
             <Col lg="auto">
-              <Form.Group controlID="name">
+              <Form.Group>
                 <Form.Label>Name</Form.Label>
-                <Form.Control type="text" placeholder="Please enter your full name"/>
+                <Form.Control required="required" type="text" placeholder="Please enter your full name" value={this.state.name} onChange={(event) => this.setState({name: event.target.value})}/>
               </Form.Group>
             </Col>
           </Row>
 
           <Row className="justify-content-md-center">
             <Col lg="auto">
-              <Form.Group controlId="subject">
+              <Form.Group >
                 <Form.Label>Subject</Form.Label>
-                <Form.Control type="text" placeholder="Subject"/>
+                <Form.Control required="required" type="text" placeholder="Subject" value={this.state.subject} onChange={(event) => this.setState({subject: event.target.value})}/>
               </Form.Group>            
             </Col>
           </Row>
 
           <Row className="justify-content-md-center">
             <Col lg="auto">
-              <Form.Group controlId="email">
+              <Form.Group controlId="formBasicEmail" >
                 <Form.Label>Email Address</Form.Label>
-                  <Form.Control type="email" placeholder="Enter email"/>
+                  <Form.Control type="email" placeholder="Enter email" value={this.state.email} onChange={(event) => this.setState({email: event.target.value})}/>
                   <Form.Text className="text">We'll never share any of your information</Form.Text>
                 
               </Form.Group>
@@ -71,9 +71,9 @@ sendEmail(e) {
 
           <Row className="justify-content-lg-center">
             <Col lg="3">
-              <Form.Group controlId="message">
+              <Form.Group >
                 <Form.Label>Message</Form.Label>
-                <Form.Control as="textarea" rows={3}  placeholder="Add your message here"/>
+                <Form.Control required="required" as="textarea" rows={3}  placeholder="Add your message here" value={this.state.message} onChange={(event) => this.setState({message: event.target.value})}/>
               </Form.Group>
             </Col>
           </Row>
@@ -90,14 +90,14 @@ sendEmail(e) {
 
         
       </Form>
-      <h2 className="contact">Tell Us How We Can Bring Your Idea To Life! </h2>
+      {/* <h2 className="contact">Tell Us How We Can Bring Your Idea To Life! </h2>
       <form  >
-        <input required="required" type="text" name="name" placeholder="Name" value={this.state.name} onChange={(event) => this.setState({name: event.target.value})}/> <br/><br/>
-        <input required="required" type="text" name="subject" placeholder="Subject" value={this.state.subject} onChange={(event) => this.setState({subject: event.target.value})}/><br/><br/>
-        <input required="required" type="email" name="email" placeholder="Email" value={this.state.email} onChange={(event) => this.setState({email: event.target.value})} /><br/><br/>
-        <textarea required="required" type="textarea" name="message" placeholder="Message" value={this.state.message} onChange={(event) => this.setState({message: event.target.value})} /><br/><br/>
+        <input  type="text" name="name" placeholder="Name" /> <br/><br/>
+        <input required="required" type="text" name="subject" placeholder="Subject" /><br/><br/>
+        <input required="required" type="email" name="email" placeholder="Email"  /><br/><br/>
+        <textarea required="required" type="textarea" name="message" placeholder="Message"  /><br/><br/>
         
-      </form> 
+      </form>  */}
     </div>
   );
 }
