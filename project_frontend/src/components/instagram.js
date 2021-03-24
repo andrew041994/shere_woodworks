af1246519d225ecdae44d79ace9e70de9f18d3a5
andrew041994 <andrew041994@gmail.com>
2021-03-23 22:57:50 -0400

    adding instagram feed

---
+++ project_frontend/src/components/instagram.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Feed from "react-instagram-authless-feed"
+
+export default class Instagram extends Component {
+  render() {
+    return (
+      <div>
+          <Feed userName="sherewoodworks" className="Feed" classNAmeLoading="Loading" limit="8"/>
+      </div>
+    );
+  }
+}
+// ReactDOM.render(
+//   <Feed userName="jamespaulmoriarty" className="Feed" classNameLoading="Loading" limit="3"/>,
+//   document.getElementById('root')
+// );ReactDOM.render(
+//     <Feed userName="jamespaulmoriarty" className="Feed" classNameLoading="Loading" limit="3"/>,
+//     document.getElementById('root')
+//   );
--- project_frontend/src/containers/Home.js
+++ project_frontend/src/containers/Home.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import  { Link } from 'react-router-dom'
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row, Col, Button, Image } from "react-bootstrap";
+import Instagram from '../components/instagram';
 
 export default class Home extends Component {
 
 
 
   render() {
+    const images = <Instagram/>
     return (
       <div >
+
+        
         <Container fluid>
          <Row className="justify-content-md-center">
           <Col md="auto"><h1 className="header">Welcome To Shere Woodworks</h1></Col>
@@ -17,7 +21,8 @@ export default class Home extends Component {
           <Col  md="auto"> <h3 className="mission">Mission Statement</h3></Col>
          </Row>
          <Row className="justify-content-md-center">
-          <Col  md="auto"><p className="statement">some stuff about yourself/company </p></Col>
+          <Col  md="auto"><p className="statement">some stuff about yourself/company </p><br/>
+          </Col>
          </Row>
          <Row className="justify-content-md-center">
           <Col  md="auto"> 
@@ -25,6 +30,11 @@ export default class Home extends Component {
           <Button>Contact Us!</Button>           
           </Link></Col>
          </Row>
+         <Row>
+          <Col xs={6} md={4}>
+         <Instagram/>
+          </Col>
+          </Row>
          
         </Container>
 
