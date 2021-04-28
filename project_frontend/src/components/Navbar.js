817b7cdb4d038caa57dd029f5d48e88d4b00ff18
andrew041994 <andrew041994@gmail.com>
2021-04-27 23:06:58 -0400

    added navbar

--- project_frontend/src/components/Navbar.js
+++ project_frontend/src/components/Navbar.js
@@ -1,30 +1,64 @@
-import React from 'react';
-// import { makeStyles } from '@material-ui/core/styles';
+import * as React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import { Avatar, makeStyles } from '@material-ui/core';
+import { Avatar} from '@material-ui/core';
 import 'fontsource-roboto';
 import img from '../images/background1.jpeg';
+import InstagramIcon from '@material-ui/icons/Instagram';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles({
     title: {
-       flexGrow: 1,
+       flexGrow: .9,
+       align: 'center',
+       color:"inherit"
+    },
+    insta: {
+        alignItems: 'right',
+        color:"inherit",
        
-    }
-}))
+        width: '40px',
+        height: '40px',
+        
+
+    },
+    logoStyle: {
+      
+        width: '68px',
+        height: '68px'
+      },
+
+      color: {
+        color:"inherit"
+      }
+
+
+})
+
+// https://www.instagram.com/sherewoodworks/
 
 const Navbar = () => {
-    const classes = useStyles
+    const classes = useStyles()
     
     return (
         <div>
-            <AppBar position="sticky">
+            <AppBar position="static">
                 <Toolbar>
-                    <Avatar onClick={() => window.location.reload(true)} alt="SW" src={img} />
-                    <Typography  align='center' variant="h6" className={classes.title}>sherewoodworks</Typography>
+                    
+                    <IconButton  >
+                        <Avatar onClick={() => window.location.reload(true)} className={classes.logoStyle} alt="SW" src={img} />
+                    </IconButton>
+
+                    <IconButton className={classes.title}>
+                        <Typography  onClick={() => window.location.reload(true)} variant="h4"   >Shere Woodworks</Typography>
+                    </IconButton>
+
+                    <IconButton className={classes.color} >
+                        <InstagramIcon onClick={() => window.open("https://www.instagram.com/sherewoodworks/")} className={classes.insta} />
+                    </IconButton>
+
                 </Toolbar>
             </AppBar>
         </div>
